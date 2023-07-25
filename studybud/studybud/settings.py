020a75f5ac4c751bce17a5f56d88d642cccace08
julianpalmerio <julianpalmerio@hotmail.com>
2023-07-25 12:02:07 -0300

    feat add templates path to settings

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            BASE_DIR / "templates",
         ],
         'APP_DIRS': True,
         'OPTIONS': {
