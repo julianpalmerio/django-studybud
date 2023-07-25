1464064c51bb20823aba6ec414e4dcb214190c7f
julianpalmerio <julianpalmerio@hotmail.com>
2023-07-25 17:23:54 -0300

    feat: add message db model

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -11,3 +11,14 @@ class Room(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.body[0:50]
