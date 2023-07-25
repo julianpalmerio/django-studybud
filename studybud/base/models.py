8934cebbbecf039988f0f287d24a1d6187372f60
julianpalmerio <julianpalmerio@hotmail.com>
2023-07-25 17:23:35 -0300

    feat: add user host to room model

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
