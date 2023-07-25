54c416321672832e0d6f0926cc979c4709cf25d0
julianpalmerio <julianpalmerio@hotmail.com>
2023-07-25 16:21:55 -0300

    feat: remove harcoded rooms info and replace them with the room database model

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,23 +1,14 @@
 from django.shortcuts import render
-
-
-rooms = [
-    {"id": 1, "name": "Lets learn Python"},
-    {"id": 2, "name": "Lets learn Django"},
-    {"id": 3, "name": "Lets learn React"},
-]
+from .models import Room
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {"rooms": rooms}
     return render(request, "base/home.html", context)
 
 
 def room(request, pk):
-    room_to_render = None
-    for room in rooms:
-        if room["id"] == int(pk):
-            room_to_render = room
-            break
-    context = {"room": room_to_render}
+    room = Room.objects.get(id=pk)
+    context = {"room": room}
     return render(request, "base/room.html", context)
