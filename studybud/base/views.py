75521687d7ec94550fbbd3b5133f689627ec39be
julianpalmerio <julianpalmerio@hotmail.com>
2023-07-25 12:58:24 -0300

    style: run black formatter

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,22 +2,22 @@ from django.shortcuts import render
 
 
 rooms = [
-    {'id': 1, 'name': 'Lets learn Python'},
-    {'id': 2, 'name': 'Lets learn Django'},
-    {'id': 3, 'name': 'Lets learn React'},
+    {"id": 1, "name": "Lets learn Python"},
+    {"id": 2, "name": "Lets learn Django"},
+    {"id": 3, "name": "Lets learn React"},
 ]
 
 
 def home(request):
-    context = {'rooms': rooms}
-    return render(request, 'base/home.html', context)
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
 
 
 def room(request, pk):
     room_to_render = None
     for room in rooms:
-        if room['id'] == int(pk):
+        if room["id"] == int(pk):
             room_to_render = room
             break
-    context = {'room': room_to_render}
-    return render(request, 'base/room.html', context)
+    context = {"room": room_to_render}
+    return render(request, "base/room.html", context)
