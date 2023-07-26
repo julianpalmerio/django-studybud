df7f4bb405a0cbcca5873031ea16f8e6c36583bf
julianpalmerio <julianpalmerio@hotmail.com>
2023-07-26 13:18:35 -0300

    feat: display messages in rooms

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 
@@ -69,7 +69,13 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {"room": room}
+    room_messages = room.message_set.all().order_by("-created")
+    if request.method == "POST":
+        message = Message.objects.create(
+            user=request.user, room=room, body=request.POST.get("body")
+        )
+        return redirect("room", pk=room.id)
+    context = {"room": room, "room_messages": room_messages}
     return render(request, "base/room.html", context)
 
 
